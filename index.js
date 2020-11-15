832610c3b6219cb52711ff99877c1c1fb66eee0b
Catheryn Small <catheryn.small@gmail.com>
2020-11-15 16:44:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+const { returns } = require("chai-spies");
+
+function submitData(username, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail
+        })
+    })
+    .then(function(rsp) {
+        return rsp.json();
+    })
+    .then(function(obj){
+        addIdToPage(obj);
+    })
+    .catch(function(error){
+        addError(error);
+    });
+};
+
+function addIdToPage(obj){
+    const div = document.getElementById('ids');
+    const li = document.createElement('li');
+    let id = obj.id;
+
+    li.innerHTML = id;
+    div.appendChild(li);
+}
+
+function addError(error){
+    const div = document.getElementById('errors');
+    const p = document.createElement('p');
+    let msg = error.message;
+
+    p.innerHTML = msg;
+    div.appendChild(p);
+}
