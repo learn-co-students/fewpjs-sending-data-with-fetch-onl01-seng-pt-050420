fd2d6c9cbc87ea63e91b65d932ae213a76a9019c
James Rogers <jarogers095@gmail.com>
2020-11-21 14:56:34 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            console.log(object);
+            appendText(object.id);
+        })
+        .catch(function(error) {
+            alert(error.message);
+            appendText(error.message);
+        })
+    ;
+
+    function appendText(text) {
+        let textElement = document.createElement("P");
+        let body = document.getElementsByTagName("body")[0];
+        textElement.textContent = text;
+        console.log(textElement);
+        body.appendChild(textElement);
+    }
+}
