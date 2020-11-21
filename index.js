a6c9ee6d024e1070363a76d3c2a534f5eb883303
danielkwon89 <danielkwon89@gmail.com>
2020-11-20 21:13:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Add your code here
-function submitData(name, email){
+\function submitData(name, email){
     return fetch('http://localhost:3000/users', {
         method: 'POST',
         headers: {
