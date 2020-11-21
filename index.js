ad3191b69a2af5295f8730c4e973433d66ab9578
kanu-e <chukwuemekakanu@gmail.com>
2020-11-21 16:17:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(function(rsp) {
+            return rsp.json();
+        })
+
+        .then (function(obj){
+            addIdToPage(obj) 
+        })
+    
+        .catch (function(error){
+            addError(error) 
+        })
+}
+
+
+function addIdToPage(obj){
+    const div = document.getElementById('ids');
+    const li = document.createElement('li');
+    let id = obj.id;
+
+    li.innerHTML = id;
+    div.appendChild(li);
+}
+
+function addError(error){
+    const div = document.getElementById('errors');
+    const p = document.createElement('p');
+    let msg = error.message;
+
+    p.innerHTML = msg;
+    div.appendChild(p);
+}
