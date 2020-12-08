c83e01582650621c109fab9b1f1168567dcf802a
Pwako11 <akolo@hotmail.com>
2020-12-08 10:44:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email){
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name, 
+            email: email
+            })
+    };
+
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(object) {
+        console.log(object);
+        document.body.innerHTML = object.id
+        })
+        .catch(function(error) {
+            alert("Your data did not load");
+            console.log(error.message);
+            document.body.innerHTML = error.message
+          });
+};
